6de24c9b343201ddadc26a56b3e4dc4bf70100fe
dan.nb <max912131@gmail.com>
2024-10-28 11:52:19 +0800

    Delete SECRET_KEY

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-crdgv@(^gt39*%#pifkil_wms-1ijzzu!masy(thi%qlfyd^#8"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
